5f7e2808b942fb0171cc8327c9943b4b9e2c3255
f <>
2021-11-21 03:30:22 +0200

    removed unnecessary routes in app.js and edited package.json script.

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import urlRouter from "./routes/url.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const port = process.env.PORT || 3013;
+const port = process.env.PORT || 3001;
 
 const app = express();
 app.set("port", port);
@@ -24,38 +24,19 @@ app.use(cors());
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-});
-
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
-app.use((req, res, next) => {
-	if (req.is("application/json") || !req.get("Content-Type")) return next();
-	return res.status(406).json({ status: "failure", message: "invalid request type" });
-});
+// set up routes and static files. order-sensitive.
 
-app.use(
-	express.json({
-		limit: "2kb",
-		verify(req, res, buf, _encoding) {
-			JSON.parse(buf);
-		},
-	}),
-);
-
-app.use(redirectRouter);
 app.use("/api/url", urlRouter);
+app.use(redirectRouter);
 
-app.all("*", (req, res) => {
-	return res.status(404).json({ status: "failure", message: "route not found" });
-});
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
 
-app.use(async (err, req, res, _next) => {
-	return res.status(500).json({ status: "failure", message: "internal error" });
-});
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+	});
+}
 
 export default app;
